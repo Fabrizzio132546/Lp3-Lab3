1b330815daabc8b2ee5db4590ce6ed4a2d6d45ea
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-09-06 22:04:56 -0500

    Update PoliticaCancelacionFlexible.java

--- Lp_Lab3/bin/Ejercicio4/PoliticaCancelacionFlexible.java
+++ Lp_Lab3/bin/Ejercicio4/PoliticaCancelacionFlexible.java
@@ -1,1 +1,18 @@
+package Ejercicio4;
+
+import java.time.LocalDate;
+
+public class PoliticaCancelacionFlexible implements PoliticaCancelacion {
+    @Override
+    public boolean puedeCancelar(Reserva reserva) {
+        LocalDate hoy = LocalDate.now();
+
+        return reserva.getInicio().isAfter(hoy.plusDays(1));
+    }
+
+    @Override
+    public double calcularPenalizacion(Reserva reserva) {
+        return 0.0; 
+    }
+}
 
