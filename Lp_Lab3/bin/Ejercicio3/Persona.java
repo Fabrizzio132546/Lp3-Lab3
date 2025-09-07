80ee47098cd59779e4c1d27422ac3161676646a5
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-09-06 21:13:41 -0500

    Update Persona.java

--- Lp_Lab3/bin/Ejercicio3/Persona.java
+++ Lp_Lab3/bin/Ejercicio3/Persona.java
@@ -1,49 +1,19 @@
 package Ejercicio3;
 
-import java.time.LocalDate;
+public abstract class Persona {
+    private String nombre;
+    private String contacto;
 
-public class Reserva {
-    private Cliente cliente;
-    private LocalDate inicio;
-    private LocalDate fin;
-    private PoliticaCancelacion politicaCancelacion;
-
-    public Reserva(Cliente cliente, LocalDate inicio, LocalDate fin, PoliticaCancelacion politica) {
-        this.cliente = cliente;
-        this.inicio = inicio;
-        this.fin = fin;
-        this.politicaCancelacion = politica;
+    public Persona(String nombre, String contacto) {
+        this.nombre = nombre;
+        this.contacto = contacto;
     }
 
-    public Cliente getCliente() {
-        return cliente;
+    public String getNombre() {
+        return nombre;
     }
 
-    public LocalDate getInicio() {
-        return inicio;
-    }
-
-    public LocalDate getFin() {
-        return fin;
-    }
-
-    public PoliticaCancelacion getPoliticaCancelacion() {
-        return politicaCancelacion;
-    }
-
-
-    public boolean seCruzaCon(LocalDate nuevoInicio, LocalDate nuevoFin) {
-        return !(nuevoFin.isBefore(inicio) || nuevoInicio.isAfter(fin));
-    }
-
-    public boolean cancelar() {
-        if (politicaCancelacion.puedeCancelar(this)) {
-            double penalizacion = politicaCancelacion.calcularPenalizacion(this);
-            System.out.println("Reserva cancelada. Penalización aplicada: " + (penalizacion * 100) + "%");
-            return true;
-        } else {
-            System.out.println("No se puede cancelar la reserva (política no lo permite).");
-            return false;
-        }
+    public String getContacto() {
+        return contacto;
     }
 }
