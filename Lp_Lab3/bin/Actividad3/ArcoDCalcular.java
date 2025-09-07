4e76507428393e3c4ded5cc8131828c9b3355af0
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-09-06 20:50:52 -0500

    Update ArcoDCalcular.java

--- Lp_Lab3/bin/Actividad3/ArcoDCalcular.java
+++ Lp_Lab3/bin/Actividad3/ArcoDCalcular.java
@@ -1,1 +1,9 @@
+package Actividad;
+
+public class ArcoDCalcular implements DamageCalculator {
+    @Override
+    public int calculate(Weapon weapon, int level) {
+        return weapon.getBaseDamage() + (level * 3);
+    }
+}
 
