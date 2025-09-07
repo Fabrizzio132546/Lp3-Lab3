9d3bd2c4325aac2ea6733702f1ac9fb64ee23410
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-09-06 21:09:54 -0500

    Update HabitacionSuite.java

--- Lp_Lab3/bin/Ejercicio3/HabitacionSuite.java
+++ Lp_Lab3/bin/Ejercicio3/HabitacionSuite.java
@@ -1,1 +1,16 @@
 
+package Ejercicio3;
+
+public class HabitacionSuite extends Habitacion {
+
+    public HabitacionSuite(String idHabitacion, double precioBase) {
+        super(idHabitacion, "Suite", precioBase);
+    }
+
+    @Override
+    public double calcularPrecioFinal(int noches) {
+
+        double recargoVIP = 50.0;
+        return (getPrecioBase() * noches) + recargoVIP;
+    }
+}
