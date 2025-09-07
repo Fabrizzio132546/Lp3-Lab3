cffb4bee3bf4166d193f33d81e2995f32a105826
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-09-06 21:07:32 -0500

    Update HabitacionIndividual.java

--- Lp_Lab3/bin/Ejercicio3/HabitacionIndividual.java
+++ Lp_Lab3/bin/Ejercicio3/HabitacionIndividual.java
@@ -1,1 +1,14 @@
+package Ejercicio3;
+
+public class HabitacionIndividual extends Habitacion {
+
+    public HabitacionIndividual(String idHabitacion, double precioBase) {
+        super(idHabitacion, "Individual", precioBase);
+    }
+
+    @Override
+    public double calcularPrecioFinal(int noches) {
+        return getPrecioBase() * noches;
+    }
+}
 
