79ea54e8b75b2a3b778a1402b7b51b50a854c8e3
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-09-06 21:06:03 -0500

    Update Habitacion.java

--- Lp_Lab3/bin/Ejercicio3/Habitacion.java
+++ Lp_Lab3/bin/Ejercicio3/Habitacion.java
@@ -1,1 +1,55 @@
+package Ejercicio3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Habitacion {
+    private String idHabitacion;
+    private String tipo;
+    private double precioBase;
+    private GestorDisponibilidadHabitacion gestorDisponibilidad;
+    private List<Reserva> reservas;
+
+    public Habitacion(String idHabitacion, String tipo, double precioBase) {
+        this.idHabitacion = idHabitacion;
+        this.tipo = tipo;
+        this.precioBase = precioBase;
+        this.reservas = new ArrayList<>();
+        this.gestorDisponibilidad = new GestorDisponibilidadHabitacion(this);
+    }
+
+    public String getIdHabitacion() {
+        return idHabitacion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public double getPrecioBase() {
+        return precioBase;
+    }
+
+    public List<Reserva> getReservas() {
+        return reservas;
+    }
+
+    public GestorDisponibilidadHabitacion getGestorDisponibilidad() {
+        return gestorDisponibilidad;
+    }
+
+    public boolean estaDisponible(java.time.LocalDate inicio, java.time.LocalDate fin) {
+        return gestorDisponibilidad.verificarDisponibilidad(inicio, fin);
+    }
+
+    public void asignarReserva(Reserva reserva) {
+        reservas.add(reserva);
+    }
+
+    public void liberarReserva(Reserva reserva) {
+        reservas.remove(reserva);
+    }
+
+    public abstract double calcularPrecioFinal(int noches);
+}
 
