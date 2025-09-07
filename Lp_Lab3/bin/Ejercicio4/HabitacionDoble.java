43ed59807a005e840faf29e057604b9d5c140e86
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-09-06 22:00:43 -0500

    Update HabitacionDoble.java

--- Lp_Lab3/bin/Ejercicio4/HabitacionDoble.java
+++ Lp_Lab3/bin/Ejercicio4/HabitacionDoble.java
@@ -1,1 +1,27 @@
 
+package Ejercicio4;
+
+public class HabitacionDoble extends Habitacion implements ServicioLimpieza, ServicioLavanderia {
+
+    public HabitacionDoble(String idHabitacion, double precioBase) {
+        super(idHabitacion, "Doble", precioBase);
+    }
+
+    @Override
+    public double calcularPrecioFinal(int noches) {
+        if (noches > 5) {
+            return (getPrecioBase() * noches) * 0.9;
+        }
+        return getPrecioBase() * noches;
+    }
+
+    @Override
+    public void solicitarLimpieza() {
+        System.out.println("Limpieza solicitada para habitación doble " + getIdHabitacion());
+    }
+
+    @Override
+    public void solicitarLavanderia(String prenda) {
+        System.out.println("Servicio de lavandería para " + prenda + " en habitación doble " + getIdHabitacion());
+    }
+}
