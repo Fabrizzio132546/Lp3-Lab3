4ef7d88f3e01af2e18a278c6bd69f1dd94b8406a
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-09-06 21:30:29 -0500

    Update PoliticaCancelacion.java

--- Lp_Lab3/bin/Ejercicio3/PoliticaCancelacion.java
+++ Lp_Lab3/bin/Ejercicio3/PoliticaCancelacion.java
@@ -1,14 +1,7 @@
 package Ejercicio3;
 
 
-public class PoliticaCancelacionEstricta implements PoliticaCancelacion {
-    @Override
-    public boolean puedeCancelar(Reserva reserva) {
-        return false; 
-    }
-
-    @Override
-    public double calcularPenalizacion(Reserva reserva) {
-        return 1.0; 
-    }
+public interface PoliticaCancelacion {
+    boolean puedeCancelar(Reserva reserva);
+    double calcularPenalizacion(Reserva reserva);
 }
