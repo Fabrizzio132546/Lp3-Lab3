c6365dea6426698d22a74da04224b1d5ae1b46e9
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-09-06 20:53:02 -0500

    Update EspadaDCalcular.java

--- Lp_Lab3/bin/Actividad3/EspadaDCalcular.java
+++ Lp_Lab3/bin/Actividad3/EspadaDCalcular.java
@@ -1,1 +1,9 @@
+package Actividad3;
+
+public class EspadaDCalcular implements DamageCalculator {
+    @Override
+    public int calculate(Weapon weapon, int level) {
+        return weapon.getBaseDamage() + (level * 5);
+    }
+}
 
