b9bb57f8dd6161d600eb81644a8af56ff0c101da
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-09-06 22:14:41 -0500

    Update PoliticaCancelacionModerada.java

--- Lp_Lab3/bin/Ejercicio4/PoliticaCancelacionModerada.java
+++ Lp_Lab3/bin/Ejercicio4/PoliticaCancelacionModerada.java
@@ -1,1 +1,18 @@
+package Ejercicio4;
+
+import java.time.LocalDate;
+
+public class PoliticaCancelacionModerada implements PoliticaCancelacion {
+    @Override
+    public boolean puedeCancelar(Reserva reserva) {
+        LocalDate hoy = LocalDate.now();
+
+        return reserva.getInicio().isAfter(hoy.plusDays(3));
+    }
+
+    @Override
+    public double calcularPenalizacion(Reserva reserva) {
+        return 0.5; 
+    }
+}
 
