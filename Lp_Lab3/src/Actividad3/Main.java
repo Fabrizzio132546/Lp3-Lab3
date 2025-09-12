40422f006f9488646a51ec625d3f3a5efb9e32c3
Lizardo Castillo <lizardo.castillo@estudiante.ucsm.edu.pe>
2025-09-12 18:57:59 -0500

    Update Main.java
    main implementado

--- Lp_Lab3/src/Actividad3/Main.java
+++ Lp_Lab3/src/Actividad3/Main.java
@@ -1,5 +1,23 @@
 package Actividad3;
 
 public class Main {
+    public static void main(String[] args) {
 
+        Weapon espada = new Espada(50);
+        Weapon arco = new Arco(30);
+
+        DamageCalculator espadaCalc = new EspadaDCalcular();
+        DamageCalculator arcoCalc = new ArcoDCalcular();
+
+        int nivel = 4;
+
+        int damageEspada = espadaCalc.calculate(espada, nivel);
+        int damageArco = arcoCalc.calculate(arco, nivel);
+
+        System.out.println("Arma: Espada,  Daño base: " + espada.getBaseDamage() +
+                           ", Nivel: " + nivel + ", Daño total: " + damageEspada);
+
+        System.out.println("Arma: Arco,  Daño base: " + arco.getBaseDamage() +
+                           ", Nivel: " + nivel + ", Daño total: " + damageArco);
+    }
 }
